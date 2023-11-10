26a244392abc49f0fc99076a59736b7adc38a591
binderiyabilguun@gmail.com <23LP0562@G302-CC0145.local>
2023-11-10 09:45:15 +0800

    nov-9

---
+++ november-9.js
@@ -0,0 +1,64 @@
+// let array = [1, 5, 7, 9, 2];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   sum = array[i] + sum;
+// }
+// console.log("sum", sum);
+// bodlog-2
+// let array = [1, 5, 7, 9, 2];
+// let total = 1;
+// for (let i = 1; i < array.length; i++) {
+//   total = array[i] + total;
+// }
+// console.log((average = total / array.length));
+// bodlog-3
+// let a = 2;
+// let b = 5;
+// let degree = 1;
+// for (let i = 0; i < b; i++) {
+//   degree = a * degree;
+// }
+// console.log(degree);
+
+// bodlog-4
+// let array = [1, 5, 7, 9, 2];
+// let max = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (max < array[i]) {
+//     max = array[i];
+//   }
+// }
+// console.log("max", max);
+
+// bodlog - 5;
+
+// let array = [1, 5, 7, 9, 2];
+// let total = 0;
+// for (let i = 0; i < array.length; i++) {
+//   total = array[i] / 2;
+// }
+// console.log(total);
+
+// let year = [1994, 1997, 2003, 1993, 2001];
+// let age = 0;
+// for (let i = 0; i < year.length; i++) {
+//   age = 2023 - year[i];
+//   console.log(age);
+// }
+
+// let n = [1, 2, 3, 4];
+// let mult = 1;
+// for (let i = 0; i < n.length; i++) {
+//   mult = n[i] * 2;
+//   console.log(mult);
+// }
+
+let n = [1, 2, 3, 4];
+let gar = 1;
+for (let i = 0; i < n.length; i++) {
+  if (i % 2 == 0) {
+    gar = n[i];
+    console.log(gar);
+  }
+}
